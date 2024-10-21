ee5d2e2bd81ad7d8cfc919bbceac732eaab8ebc4
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-21 17:03:27 +0600

    Added a link to bootstrap & bootstrap icons, added axios default url & wrapped the App into BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter } from 'react-router-dom';
 
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://restcountries.com/v2/';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
